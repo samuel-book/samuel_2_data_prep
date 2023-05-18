b610eddc9fca699ffa03567037fbc4e0fd898cc1
amyheather <a.heather2@exeter.ac.uk>
2023-05-18 09:52:47 +0100

    Removed redundant tests (i.e. as changed from ifelse to map in reformat data, we explicit state what to map from raw data to clean, and hence it is redundant to again test for this). Keeping tests for e.g. negative results, unusual values.

--- tests/test_01_reformat_data.py
+++ tests/test_01_reformat_data.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import pandas as pd
 import unittest
@@ -14,9 +13,9 @@ class DataTests(unittest.TestCase):
     def setUpClass(self):
         '''Set up - runs prior to each test'''
         # Paths and filenames
-        raw_path: str = './data'
+        raw_path: str = './../data'
         raw_filename: str = 'SAMueL ssnap extract v2.csv'
-        clean_path: str = './output'
+        clean_path: str = './../output'
         clean_filename: str = 'reformatted_data.csv'
         # Import dataframes
         raw_data = pd.read_csv(os.path.join(raw_path, raw_filename),
@@ -26,26 +25,6 @@ class DataTests(unittest.TestCase):
         # Save to DataTests class
         self.raw = raw_data
         self.clean = clean_data
-
-    def freq(self, raw_col, raw_val, clean_col, clean_val):
-        '''
-        Test that the frequency of a value in the raw data is same as
-        the frequency of a value in the cleaned data
-        Inputs:
-        - self
-        - raw_col and clean_col = string
-        - raw_val and clean_val = string, number, or list
-        Performs assertEqual test.
-        '''
-        # If values are not lists, convert to lists
-        if type(raw_val) != list:
-            raw_val = [raw_val]
-        if type(clean_val) != list:
-            clean_val = [clean_val]
-        # Find frequencies and check if equal
-        raw_freq = (self.raw[raw_col].isin(raw_val).values).sum()
-        clean_freq = (self.clean[clean_col].isin(clean_val).values).sum()
-        self.assertEqual(raw_freq, clean_freq)
 
     def time_neg(self, time_column):
         '''
@@ -76,46 +55,6 @@ class DataTests(unittest.TestCase):
         self.assertEqual(len(self.clean.id.unique()),
                          len(self.clean.index))
 
-    def test_gender(self):
-        '''Test the number of each gender has not changed'''
-        self.freq('S1Gender', 'M', 'male', 1)
-        self.freq('S1Gender', 'F', 'male', 0)
-
-    def test_stroke_type(self):
-        '''Test the number of each gender has not changed'''
-        self.freq('S2StrokeType', 'I', 'infarction', 1)
-        self.freq('S2StrokeType', 'PIH', 'infarction', 0)
-
-    def test_onset(self):
-        '''Test that onset_known is equal to precise + best estimate'''
-        self.freq('S1OnsetTimeType', ['P', 'BE'], 'onset_known', 1)
-        self.freq('S1OnsetTimeType', 'NK', 'onset_known', 0)
-
-    def test_precise_onset(self):
-        '''Test that precise_onset_known is 1 for P and 0 for BE + NK'''
-        self.freq('S1OnsetTimeType', 'P', 'precise_onset_known', 1)
-        self.freq('S1OnsetTimeType', ['BE', 'NK'], 'precise_onset_known', 0)
-
-    def test_sleep(self):
-        '''Test that onset during sleep values are consistent'''
-        self.freq('S1OnsetDateType', 'DS', 'onset_during_sleep', 1)
-        self.freq('S1OnsetDateType', ['P', 'BE'], 'onset_during_sleep', 0)
-
-    def test_by_ambulance(self):
-        '''Test if numbers arriving by ambulance are correct'''
-        self.freq('S1ArriveByAmbulance', 'Y', 'arrive_by_ambulance', 1)
-        self.freq('S1ArriveByAmbulance', 'N', 'arrive_by_ambulance', 0)
-
-    def test_thrombolysis(self):
-        '''Test that numbers receiving thrombolysis are correct'''
-        self.freq('S2Thrombolysis', 'Y', 'thrombolysis', 1)
-        self.freq('S2Thrombolysis', ['N', 'NB'], 'thrombolysis', 0)
-
-    def test_thrombectomy(self):
-        '''Test that number not receiving thrombectomy equals number NaN'''
-        self.freq('ArrivaltoArterialPunctureMinutes', np.nan,
-                  'thrombectomy', 0)
-
     def test_time_negative(self):
         '''Test that times are not negative when expected to be positive'''
         time_col = ['onset_to_arrival_time',
@@ -140,40 +79,6 @@ class DataTests(unittest.TestCase):
             (self.clean['ambulance_travel_to_hospital_time'].notnull()) |
             (self.clean['ambulance_wait_time_at_hospital'].notnull()))]
         self.assertEqual(len(amb_neg.index), 0)
-
-    def test_comorbid_yn(self):
-        '''Test that comorbidity numbers are as expected (Y, N)'''
-        col_list = (
-            [['S2CoMCongestiveHeartFailure', 'congestive_heart_failure'],
-             ['S2CoMHypertension', 'hypertension'],
-             ['S2CoMAtrialFibrillation', 'atrial_fibrillation'],
-             ['S2CoMDiabetes', 'diabetes'],
-             ['S2CoMStrokeTIA', 'prior_stroke_tia']])
-        for raw_col, clean_col in col_list:
-            with self.subTest(msg=clean_col):
-                self.freq(raw_col, 'Y', clean_col, 1)
-                self.freq(raw_col, 'N', clean_col, 0)
-
-    def test_comorbid_nb(self):
-        '''Test that comorbidity numbers are as expected (Y, N, NB)'''
-        col_list = (
-            [['S2CoMAFAntiplatelet', 'afib_antiplatelet'],
-             ['S2CoMAFAnticoagulent', 'afib_anticoagulant']])
-        for raw_col, clean_col in col_list:
-            with self.subTest(msg=clean_col):
-                self.freq(raw_col, 'Y', clean_col, 1)
-                self.freq(raw_col, ['N', 'NB', np.nan], clean_col, 0)
-
-    def test_comorbid_01(self):
-        '''Test that comorbidity numbers are as expected (1, 0)'''
-        col_list = (
-            [['S2CoMAFAnticoagulentVitK', 'afib_vit_k_anticoagulant'],
-             ['S2CoMAFAnticoagulentDOAC', 'afib_doac_anticoagulant'],
-             ['S2CoMAFAnticoagulentHeparin', 'afib_heparin_anticoagulant']])
-        for raw_col, clean_col in col_list:
-            with self.subTest(msg=clean_col):
-                self.freq(raw_col, 1, clean_col, 1)
-                self.freq(raw_col, [0, np.nan], clean_col, 0)
 
     def test_nihss_min_max(self):
         '''Test that minimum and maximum NIHSS scores are within bounds'''
@@ -202,10 +107,6 @@ class DataTests(unittest.TestCase):
         for col, values in expected:
             with self.subTest(msg=col):
                 self.equal_array(self.clean, col, values)
-
-    def test_death(self):
-        '''Test number died is as expected'''
-        self.freq('ArrivalToDeathDays', np.nan, 'death', 0)
 
     def test_no_thrombolysis(self):
         '''
